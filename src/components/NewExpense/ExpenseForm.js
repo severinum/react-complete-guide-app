86e9502dadcabb10e3335d3c753e24ca36cd041d
SeverinuM <sewerynmichota@gmail.com>
2021-09-16 15:08:10 +0100

    Fetching form data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -19,23 +19,43 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     };
 
+    const submitHander = (event) => {
+        event.preventDefault();
+        
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHander} >
             <div className="new-expense__controls">
 
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}  />
+                    <input type="text" 
+                    value={enteredTitle} 
+                    onChange={titleChangeHandler}  />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number"  min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number"  min="0.01" step="0.01" 
+                    value={enteredAmount} 
+                    onChange={amountChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="date" min="2019-01-01" max="2025-12-31" onChange={dateChangeHandler} />
+                    <input type="date" min="2019-01-01" max="2025-12-31" 
+                    value={enteredDate} 
+                    onChange={dateChangeHandler} />
                 </div>
 
             </div>
